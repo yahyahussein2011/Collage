099416e937fb66fff831a34896987926ec022262
yahyahussein2011 <121323738+yahyahussein2011@users.noreply.github.com>
2023-08-26 18:49:45 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event) {
+
+    console.log(event);
+
+    var Content = event.results[0][0].transcript;
+    console.log(Content);
+
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+}
+
+
+function speak(){
+    var synth = window.speechSyntheseis;
+
+    speak_data = document.getElementById("textbox").value;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+camera=document.getElementById("camera");
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format:'png',
+    png_quality:90
+});
